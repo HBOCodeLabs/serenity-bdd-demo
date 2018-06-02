2387b75a7b6e9d712e8115fe6789f1a3dee2d5a2
Charles Fulnecky <charles.fulnecky@hbo.com>
2018-06-02 11:16:20 -0400

    Make webstorm's linter happy.

--- spec/screenplay/tasks/go/ask_for_a_license_plate_code.ts
+++ spec/screenplay/tasks/go/ask_for_a_license_plate_code.ts
@@ -1,7 +1,6 @@
 import {Task} from '@serenity-js/core/lib/screenplay';
 import {Get} from '../../../support/interactions/get';
 
-
 export const AskForALicensePlateCode = () => Task.where(`#actor wants to get a License Plate Code`,
    Get.resource('/'),
 );
